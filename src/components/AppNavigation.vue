01438013226a46f331cb5c2e78224ab02558210a
Coucougnou <69139661+Coucougnou@users.noreply.github.com>
2020-11-10 09:47:54 +0100

    Add files via upload

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,45 @@
+<template>
+<span>
+        <v-navigation-drawer app v-model="drawer" class="indigo darken-4" dark disable-resize-watcher>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-tile :key="index">
+                        <v-list-tile-content>
+                            {{item.title}}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+<v-toolbar app color="indigo darken-4" dark>
+    <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+    <v-spacer class="hidden-md-and-up"></v-spacer>
+    <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+    <v-btn text class="hidden-sm-and-down">Menu</v-btn>
+    <v-spacer class="hidden-sm-and-down"></v-spacer>
+    <v-btn text class="hidden-sm-and-down">SIGN IN</v-btn>
+    <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+</v-toolbar>
+</span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+      data() {
+        return {
+            appTitle: 'Culture-Map',
+            drawer: false,
+            items: [
+                { title: 'Menu' },
+                { title: 'Sign In' },
+                { title: 'Join' }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/HomePlans.vue
@@ -0,0 +1,150 @@
+<template>
+    <v-container grid-list-lg>
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Site Culturels disponibles :</v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/DfRhinJk7fw" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Lieux de Culte</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Lieux de culte</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/RLQ0YYzQuis" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Musées</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Musées</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/m3th3rIQ9-w" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Théâtres</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Théâtres</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <v-container grid-list-lg>
+        
+        <v-layout row wrap>
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/z99Z1SL67HE" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Parcs</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Parcs</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/OcKeEdgzVuU" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Boîtes de Nuit</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Boîtes de Nuit</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="http://source.unsplash.com/JNTSoyb_bbw" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">Galleries d'Art</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Galleries d'Art</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+    </v-container>
+    
+</template>
+
+<script>
+export default {
+    name: 'HomePlans'
+};
+</script>
+
+<style scoped>
+</style>
